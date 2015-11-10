808bfe79515da1f12ccf06084a6d7cdb7b4500d5
ryanAndersen05 <ryan.andersen05@gmail.com>
2015-11-10 13:10:53 -0500

    Truck added to demo

---
+++ Mini Project/Assets/Scripts/GameMechanics/DrivingMechanics.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DrivingMechanics : MonoBehaviour {
+    public float maxSpeed;
+    public float acceleration;
+
+    Rigidbody rigid;
+    float inputSpeed;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        inputSpeed = Input.GetAxisRaw("Vertical");
+
+        updateMovement();
+    }
+
+    void updateMovement()
+    {
+        Vector3 goalVel = transform.forward * maxSpeed * inputSpeed;
+
+        rigid.velocity = Vector3.Lerp(rigid.velocity, goalVel, Time.deltaTime * acceleration);
+    }
+
+}
