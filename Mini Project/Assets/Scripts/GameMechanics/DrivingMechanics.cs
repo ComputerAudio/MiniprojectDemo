3ec3a7adaf9bb3fc4d938be45d373d10a612023f
ryanAndersen05 <ryan.andersen05@gmail.com>
2015-11-11 10:49:13 -0500

    Basic turn mechanics added

--- Mini Project/Assets/Scripts/GameMechanics/DrivingMechanics.cs
+++ Mini Project/Assets/Scripts/GameMechanics/DrivingMechanics.cs
@@ -4,9 +4,11 @@ using System.Collections;
 public class DrivingMechanics : MonoBehaviour {
     public float maxSpeed;
     public float acceleration;
+    public float turnRate;
 
     Rigidbody rigid;
     float inputSpeed;
+    float inputTurn;
 
     void Awake()
     {
@@ -16,15 +18,29 @@ public class DrivingMechanics : MonoBehaviour {
     void Update()
     {
         inputSpeed = Input.GetAxisRaw("Vertical");
+        inputTurn = Input.GetAxisRaw("Horizontal");
 
         updateMovement();
+        updateRotation();
     }
 
     void updateMovement()
     {
+        if (inputSpeed < .001f)
+        {
+            return;
+        }
         Vector3 goalVel = transform.forward * maxSpeed * inputSpeed;
 
         rigid.velocity = Vector3.Lerp(rigid.velocity, goalVel, Time.deltaTime * acceleration);
+    }
+
+    void updateRotation()
+    {
+        float currentRotation = transform.eulerAngles.y;
+        float goalRotation = currentRotation + inputTurn * 90;
+        Vector3 newDirection = new Vector3(transform.eulerAngles.x, Mathf.MoveTowards(currentRotation, goalRotation, Time.deltaTime * turnRate), transform.eulerAngles.z);
+        transform.rotation = Quaternion.Euler(newDirection);
     }
 
 }
