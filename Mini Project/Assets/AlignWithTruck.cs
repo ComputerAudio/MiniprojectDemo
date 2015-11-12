17b0fdf4c666083f4cbebfbb05f398ebd620194a
ryanAndersen05 <ryan.andersen05@gmail.com>
2015-11-11 20:51:20 -0500

    hinge joint added to truck

---
+++ Mini Project/Assets/AlignWithTruck.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlignWithTruck : MonoBehaviour {
+    public DrivingMechanics driveMechanics;
+    Rigidbody rigid;
+
+	// Use this for initialization
+	void Start () {
+        rigid = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float goalRotation = driveMechanics.transform.eulerAngles.y;
+        Vector3 newEuler = new Vector3(driveMechanics.transform.eulerAngles.x, Mathf.MoveTowards(transform.eulerAngles.y, goalRotation, Time.deltaTime * rigid.velocity.magnitude),
+            driveMechanics.transform.eulerAngles.z);
+
+	}
+}
