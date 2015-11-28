d44d4680581016192841c3165ee2f07aaea0e13d
Jesse Wu <jwu308@gatech.edu>
2015-11-27 20:49:39 -0700

    Added wheel status UI
    Wheel color changes according to the wheel's safety status. Dynamic
    color range between red to yellow to green (0.0 to 1.0). Red = dangerous
    (low traction, thin ice below) and Green = safe (high traction, thick
    stable ice below)

---
+++ Mini Project/Assets/Scripts/WheelStatus.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class WheelStatus : MonoBehaviour {
+	public float fragility;
+
+	Image myWheel;
+
+	// Use this for initialization
+	void Start () {
+		myWheel = GetComponent<Image> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		myWheel.color = new Vector4 (fragility, (1 - fragility), 0, 1);
+	}
+
+}
