0817e7fa5b3f893091b38f8991bfe191d994197a
ryanAndersen05 <ryan.andersen05@gmail.com>
2015-12-06 17:26:53 -0500

    Thermometer added to game

---
+++ Mini Project/Assets/Scripts/ThermometerManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThermometerManager : MonoBehaviour {
+    public TemperatureManager manager;
+    TextMesh text;
+
+    void Start()
+    {
+        text = GetComponent<TextMesh>();
+    }
+
+    void Update()
+    {
+        int temp = (int)manager.currentTemperature;
+        text.text = "" + temp;
+    }
+}
---
+++ Mini Project/Assets/Scripts/ThermometerScale.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThermometerScale : MonoBehaviour {
+    public TemperatureManager manager;
+    float scaleY;
+
+    void Update()
+    {
+        float currentTemp = manager.currentTemperature;
+        currentTemp = currentTemp + 70;
+        currentTemp = currentTemp / 40f * .5f;
+        Vector3 scale = transform.localScale;
+        transform.localScale = new Vector3(scale.x, currentTemp, scale.z);
+
+    }
+
+}
