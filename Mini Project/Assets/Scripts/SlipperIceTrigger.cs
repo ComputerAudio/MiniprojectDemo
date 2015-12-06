01df3b60416c449c83779a334f0cae3237a8ac2c
ryanAndersen05 <ryan.andersen05@gmail.com>
2015-12-06 12:29:53 -0500

    Slippery Ice Trigger added

---
+++ Mini Project/Assets/Scripts/SlipperIceTrigger.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlipperIceTrigger : MonoBehaviour {
+    public float driftSpeed;
+    float originalDriftSpeed;
+
+    void OnTriggerEnter(Collider collider)
+    {
+        DrivingMechanics mechanics = collider.GetComponent<DrivingMechanics>();
+        if (mechanics != null)
+        {
+            originalDriftSpeed = mechanics.turnRate;
+            driftSpeed = originalDriftSpeed * 5;
+            mechanics.turnRate = driftSpeed;
+        }
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+        DrivingMechanics mechanics = GetComponent<DrivingMechanics>();
+        if (mechanics != null)
+        {
+            mechanics.turnRate = originalDriftSpeed;
+        }
+    }
+}
