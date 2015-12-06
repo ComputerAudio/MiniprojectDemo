db83a741a33928d5e4008eb0d5b95fe1da508b63
ryanAndersen05 <ryan.andersen05@gmail.com>
2015-12-06 13:10:01 -0500

    Temperature scripts written

---
+++ Mini Project/Assets/Scripts/TemperatureManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TemperatureManager : MonoBehaviour {
+    public float currentTemperature = -32;
+    AudioSource aSource;
+
+    void Start()
+    {
+        aSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+
+    }
+}
---
+++ Mini Project/Assets/Scripts/TemperatureTrigger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TemperatureTrigger : MonoBehaviour {
+    public TemperatureManager tempManager;
+    public float triggerTemperature;
+
+    void OnTriggerEnter(Collider collider)
+    {
+        tempManager.currentTemperature = triggerTemperature;
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+
+    }
+}
