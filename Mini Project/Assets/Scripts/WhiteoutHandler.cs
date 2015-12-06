20e47dac76fd1177f5f26bd374bf578c78465d90
ryanAndersen05 <ryan.andersen05@gmail.com>
2015-12-06 02:32:23 -0500

    Blinker logic added to truck

--- Mini Project/Assets/Scripts/WhiteoutHandler.cs
+++ Mini Project/Assets/Scripts/WhiteoutHandler.cs
@@ -1,15 +1,52 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class WhiteoutHandler : MonoBehaviour {
+    public SpriteRenderer leftArrow;
+    public SpriteRenderer rightArrow;
+    public float speedOfBlinker = 3f;
+    public float blinkerOnTime = 2f;
+    public Transform playerRefernce;
+    float blinkerTimer;
+    bool snowStormOn;
+    //bool blinkerOn;
+	
+    void Start()
+    {
+        playerRefernce = GameObject.FindGameObjectWithTag("Player").transform;
+    }
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
 	// Update is called once per frame
 	void Update () {
-	
+        updateBlinker();
 	}
+
+    void updateBlinker()
+    {
+        blinkerTimer = Mathf.MoveTowards(blinkerTimer, 0, Time.deltaTime);
+        if (snowStormOn)
+        {
+            if (blinkerTimer <= 0)
+            {
+                blinkerTimer = speedOfBlinker;
+            }
+            if (blinkerTimer <= blinkerOnTime)
+            {
+                leftArrow.color = Color.green;
+            }
+            else
+            {
+                leftArrow.color = Color.white;
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "Player")
+        {
+            snowStormOn = true;
+        }
+    }
 }
